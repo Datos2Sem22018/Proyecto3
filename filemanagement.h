e3a73569f23bfe4e9e1985dbe59b15ebe9804a13
zuckerberg <prieto.luisdaniel@gmail.com>
2018-11-24 20:10:54 -0600

    File Manager actualizado

---
+++ filemanagement.h
@@ -0,0 +1,26 @@
+#ifndef FILEMANAGEMENT_H
+#define FILEMANAGEMENT_H
+
+#include <fstream>
+#include <iostream>
+#include <memory>
+#include <sstream>
+#include <vector>
+#include <sys/stat.h>
+
+class FileManagement {
+public:
+    void writeFile(std::string file, std::string text);
+    void copy_file(std::string pathSrc, std::string pathDest);
+    std::unique_ptr<std::ofstream> createChunkFile(std::vector<std::string>& vecFilenames);
+    void split(const std::string& text, std::istream& inStream, int nMegaBytesPerChunk, std::vector<std::string>& vecFilenames);
+    void join(std::vector<std::string>& vecFilenames, std::ostream& outStream);
+    float GetFileSize(const std::string& filename);
+    template<typename T, typename... Args>
+    std::unique_ptr<T> make_unique(Args&&... args)
+    {
+        return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
+    }
+};
+
+#endif // FILEMANAGEMENT_H
